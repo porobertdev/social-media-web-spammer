a1a7c25ab4299e149dcd7b8681d832ef1dd2f9ae
Robert P. <porobertdev@gmail.com>
2024-05-12 15:10:11 +0300

    Add support for Telegram
    And:
    * switch to classes
    * check for the spamming mode
    * save the input element, msg etc. on the instance of the Spammer
    * add a wrapper function for the setTimeout to avoid loop issues
    * use the `keyup` event as it's more accurate when using SHIFT key to add symbols.

--- spam.js
+++ spam.js
@@ -8,7 +8,7 @@ class API {
     }
 
     async getResponse() {
-        console.log('Getting Chuck Norris quote...');
+        console.log(`[FETCHING] API: ${this.url}`);
         const response = await fetch(this.url);
         const data = await response.json();
 
@@ -21,74 +21,87 @@ class API {
 }
 
 const chuckAPI = new API('https://api.chucknorris.io/jokes/random');
-const selector =
-    'div._ak1l .x1hx0egp.x6ikm8r.x1odjw0f.x1k6rcq7.x6prxxf[contenteditable="true"]';
 
-const input = document.querySelector(selector);
+class Spammer {
+    constructor(selector) {
+        this.selector = selector;
+    }
 
-function spam(msg) {
-    console.log('[SPAMMER] Writing message...');
-    const promise = new Promise((resolve, reject) => {
-        if (input) {
-            resolve(true);
+    async spam() {
+        console.log('[SPAMMER] Writing message...');
+
+        if (this.mode == 'api') {
+            await chuckAPI.getResponse();
+            this.msg = chuckAPI.quote;
         }
-    });
 
-    promise
-        .then((result) => {
-            // fill the input box
-            input.dispatchEvent(
-                new InputEvent('beforeinput', {
-                    inputType: 'insertText',
-                    data: msg,
-                    bubbles: true,
-                    cancelable: true,
-                })
-            );
-        })
-        .then((val) => {
-            // send message
-            input.dispatchEvent(
-                new KeyboardEvent('keydown', {
-                    key: 'Enter',
-                    code: 'Enter',
-                    which: 13,
-                    keyCode: 13,
-                })
-            );
+        const promise = new Promise((resolve, reject) => {
+            if (this.input) {
+                resolve(true);
+            }
         });
-}
 
-function start(msg, times) {
-    console.log('[SPAMMER[ Starting...');
-
-    for (i = 0; i < times; i++) {
-        // we need a timeout because otherwise dispatchEvent(kb) doesn't have enough time to run
-        setTimeout(() => {
-            spam(msg);
-        }, 500);
+        promise
+            .then((result) => {
+                // fill the input box
+                this.input.textContent = this.msg;
+            })
+            .then((val) => {
+                // send message
+                this.input.dispatchEvent(
+                    new KeyboardEvent('keydown', {
+                        key: 'Enter',
+                        code: 'Enter',
+                        which: 13,
+                        keyCode: 13,
+                    })
+                );
+            });
     }
-}
 
-input.addEventListener('input', (event) => {
-    const splitted = event.target.textContent.split('"');
-    const [message, times] = [splitted[1], +splitted[2].trim()];
-    console.log('🚀 ~ input.addEventListener ~  [message, times]:', [
-        message,
-        times,
-    ]);
+    start() {
+        console.log('starting');
+        const setTimeoutWrapper = () => {
+            // we need a timeout because otherwise dispatchEvent(kb) doesn't have enough time to run
+            setTimeout(() => {
+                this.spam();
+            }, 1500);
+        };
 
-    if (splitted.length == 3) {
-        switch (true) {
-            // @spam "some text" 50
-            case splitted[0].includes('@spam'):
-                start(message, times);
-                break;
-
-            // @api "chuck" 50
-            case splitted[0].includes('@api'):
-                console.log('starting api chuck');
-                start(chuckAPI.getResponse(), times);
+        for (let i = 0; i < this.times; i++) {
+            console.log(this.times);
+            setTimeoutWrapper();
         }
     }
-});
+
+    addEvent() {
+        // add event listener
+        this.input = document.querySelector(this.selector);
+        this.input.addEventListener('keyup', (event) => {
+            const splitted = event.target.textContent.split('"');
+            console.log(splitted);
+
+            if (splitted.length == 3 && splitted.join(' ').endsWith('start')) {
+                // msg / api
+                this.mode = splitted[0].split(' ')[1];
+
+                // actual message or API name
+                this.msg = splitted[1];
+
+                // number
+                this.times = +splitted[2].split(' ')[1].trim();
+
+                this.start();
+            }
+        });
+    }
+}
+
+const whatsapp = new Spammer(
+    'div._ak1l .x1hx0egp.x6ikm8r.x1odjw0f.x1k6rcq7.x6prxxf[contenteditable="true"]'
+);
+const telegram = new Spammer(
+    '.input-message-input.scrollable.scrollable-y.no-scrollbar'
+);
+
+telegram.addEvent();
